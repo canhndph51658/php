11cbab4eb2f459475178670370ab76a2f76071c4
canhnd <canhndph51658@gmail.com>
2025-05-09 19:27:20 +0700

    Add files via upload

---
+++ status/includes/header.php
@@ -0,0 +1,10 @@
+<!-- includes/header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhật ký Hoạt động Hệ thống</title>
+</head>
+<body>
+    <h1>Hệ thống nhật ký hoạt động</h1>
